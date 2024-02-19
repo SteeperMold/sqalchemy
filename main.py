d9b6878a3b392d0e9f4bb5d7cc73e580560b9d74
Leamich <tennantonelove@yandex.ru>
2024-02-19 18:52:11 +0400

    Первая работа

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
 from data import db_session
-from data.users import User
+from data.users import User, Jobs
 
 
 def main():
@@ -24,6 +25,15 @@ def main():
         new_user.address = address
         new_user.email = email
         db_sess.add(new_user)
+
+    new_job = Jobs()
+    new_job.team_leader = 1
+    new_job.job = 'deployment of residential modules 1 and 2'
+    new_job.work_size = 15
+    new_job.collaborators = '2, 3'
+    new_job.start_date = datetime.datetime.now()
+    new_job.is_finished = False
+    db_sess.add(new_job)
 
     db_sess.commit()
 
