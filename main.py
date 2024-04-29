5863e72044c6cd0ed17ffdb25b690837599c6f65
SteeperMold <112201965+SteeperMold@users.noreply.github.com>
2024-04-29 11:21:43 +0400

    commit

--- main.py
+++ main.py
@@ -111,43 +111,6 @@ def load_user(user_id):
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-
-    import os
-
-    if not os.path.exists(os.path.realpath('db/blogs.db')):
-
-        users = [
-            ["Scott", "Riddley", 21, 'captain', 'research engineer', 'module_1', "scott_chief@mars.org"],
-            ["Scott2", "Riddley", 22, 'not captain', 'research engineer', 'module_2', "scott2_chief@mars.org"],
-            ["Scott3", "Riddley", 23, 'not captain', 'research engineer', 'module_3', "scott3_chief@mars.org"],
-            ["Scott4", "Riddley", 24, 'not captain', 'research engineer', 'module_4', "scott4_chief@mars.org"],
-        ]
-
-        db_sess = db_session.create_session()
-
-        for surname, name, age, position, speciality, address, email in users:
-            new_user = User()
-            new_user.surname = surname
-            new_user.name = name
-            new_user.age = age
-            new_user.position = position
-            new_user.speciality = speciality
-            new_user.address = address
-            new_user.email = email
-            db_sess.add(new_user)
-
-        for i in range(5):
-            new_job = Jobs()
-            new_job.team_leader = 1
-            new_job.job = 'deployment of residential modules 1 and 2'
-            new_job.work_size = 15
-            new_job.collaborators = '2, 3'
-            new_job.start_date = datetime.datetime.now()
-            new_job.is_finished = False
-            db_sess.add(new_job)
-
-        db_sess.commit()
-
     app.register_blueprint(jobs_bp)
     app.register_blueprint(users_bp)
     app.run()
